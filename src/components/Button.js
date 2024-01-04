3a77d7c2ccf8263bfc024fac805cb7ddf9dbb0b3
Merve Balci <mervebalci090@gmail.com>
2024-01-03 22:40:06 -0400

    Styling some of the calculator buttons with CSS

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,18 @@
+function getStyleName(btn) {
+  const className = {
+    "C": "clear",
+    "=": "equals",
+    "+": "opt",
+    "-": "opt",
+    "x": "opt",
+    "÷": "opt",
+  }
+  return className[btn]
+}
+
 export default function Button({ value }) {
   return (
-    <button  className="button">
+    <button className={`${getStyleName(value)} button`}>
       {value}
     </button>
   )
